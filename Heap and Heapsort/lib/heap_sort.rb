41116b76eaaf5453c4230602f7251b48165d858d
Jo Jangmi <wkdal720@gmail.com>
2016-09-27 06:46:33 -0700

    heap sort completed

--- Heap and Heapsort/lib/heap_sort.rb
+++ Heap and Heapsort/lib/heap_sort.rb
@@ -1,27 +1,21 @@
 require_relative "heap"
 
 class Array
-  def heap_sort!
-    heap = BinaryMinHeap.new { |a, b| b <=> a }
-    store = []
-    each { |el| store = heap.push(el) } # build heap and store in store
+  def heap_sort!(&prc)
+    # set default proc
+    prc ||= proc { |a, b| a <=> b }
+    # build the heap with original array
+    (size - 1).downto(0).each do |i|
+      # do the opposite way sort to optimize time complexsity
+      BinaryMinHeap.heapify_up(self, i, size) { |a, b| -prc.call(a, b) }
+    end
+    # set the boundary
     i = 0
     j = size - 1
     while i != j
-      store[i], store[j] = store[j], store[i]
-      # p "#{i}, #{j}"
+      self[i], self[j] = self[j], self[i]
+      BinaryMinHeap.heapify_down(self, 0, j) { |a, b| -prc.call(a, b) }
       j -= 1
-      store = BinaryMinHeap.heapify_down(store, 0, j)
-      #TODO this line is not happening!!
     end
-    store
   end
 end
-
-p [5, 4, 3, 2, 1].heap_sort!
-# heap = BinaryMinHeap.new
-# 5.downto(1).each do |i|
-#   heap.push(i)
-# end
-#
-# p BinaryMinHeap.heapify_down([1, 2, 4, 5, 3], 1, 4)
