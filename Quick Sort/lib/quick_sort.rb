ca6b1f0845d121a07323c7523c7919a33e196fed
Jo Jangmi <wkdal720@gmail.com>
2016-09-28 07:19:33 -0700

    refactor

--- Quick Sort/lib/quick_sort.rb
+++ Quick Sort/lib/quick_sort.rb
@@ -5,27 +5,23 @@ class QuickSort
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
     return array if array.size <= 1
-    pivot = array.shift
-    left = []
-    right = []
-    array.each do |el|
-      if el < pivot
-        left << el
-      else
-        right << el
-      end
-    end
-    QuickSort.sort1(left).concat([pivot]).concat(QuickSort.sort1(right))
+    pivot_idx = rand(array.size)
+
+    left = array.select { |el| el < array[pivot_idx] }
+    middle = array.select { |el| el == array[pivot_idx] }
+    right = array.select { |el| el > array[pivot_idx] }
+    sort1(left).concat(middle).concat(sort1(right))
   end
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
     return array if length <= 1
-    pivot = QuickSort.partition(array, start, length, &prc)
-    left_len = pivot - start
+    pivot_idx = partition(array, start, length, &prc)
+    left_len = pivot_idx - start
     right_len = length - left_len - 1
-    QuickSort.sort2!(array, start, left_len, &prc)
-    QuickSort.sort2!(array, pivot + 1, right_len, &prc)
+    # right length = subtract left length and pivot
+    sort2!(array, start, left_len, &prc)
+    sort2!(array, pivot_idx + 1, right_len, &prc)
   end
 
   def self.partition(array, start, length, &prc)
@@ -33,6 +29,13 @@ class QuickSort
     pivot_idx = start
     (start + 1).upto(length + start - 1).each do |i|
       next unless prc.call(array[pivot_idx], array[i]) == 1
+      # val = array[i]
+      # array[i] = array[pivot_idx + 1]
+      # array[pivot_idx + 1] = array[pivot_idx]
+      # array[pivot_idx] = val
+
+      # I like this way better : )
+      # slightly uses less memory and code lines
       array[pivot_idx], array[i] = array[i], array[pivot_idx]
       array[pivot_idx + 1], array[i] = array[i], array[pivot_idx + 1]
       pivot_idx += 1
