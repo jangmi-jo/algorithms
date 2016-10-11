06c0b417501e58713867c8f276e80f4d5b6bc5e6
Jo Jangmi <wkdal720@gmail.com>
2016-10-10 21:35:07 -0700

    bfs_graph

--- coding challenges/bfs_graph.js
+++ coding challenges/bfs_graph.js
@@ -2,6 +2,7 @@
 const processData = (input) => {
   let arr = input.split("\n");
   let q = Number(arr.shift());
+  // console.time('input');
   for (let i=0; i<q; i++) {
     let nm = arr.shift().split(" ").map(Number);
     let edges = [];
@@ -9,6 +10,7 @@ const processData = (input) => {
       edges.push(arr.shift().split(" ").map(Number));
     }
     let start = Number(arr.shift());
+    // console.timeEnd('input');
     console.log(shortestPath(nm[0], edges, start));
   }
 };
@@ -19,39 +21,57 @@ const generateEdgeHash = (edges) => {
     if (!edgeHash[e[0]]) {
       edgeHash[e[0]] = [];
     }
+    if (!edgeHash[e[1]]) {
+      edgeHash[e[1]] = [];
+    }
     edgeHash[e[0]].push(e[1]);
+    edgeHash[e[1]].push(e[0]);
   });
   return edgeHash;
 };
+//
+// const generateEdgeHash = (edges) => {
+//   return edges.reduce((memo, e) => {
+//     if (!memo[e[0]]) {
+//       memo[e[0]] = [];
+//     }
+//     if (!memo[e[1]]) {
+//       memo[e[1]] = [];
+//     }
+//     memo[e[0]].push(e[1]);
+//     memo[e[1]].push(e[0]);
+//     return memo;
+//   }, {});
+// };
+
 
 const shortestPath = (numOfNodes, edges, start) => {
+  console.time('edgeHash');
   let edgeHash = generateEdgeHash(edges);
-  let visited = {};
-  let distanceHash = {};
-  distanceHash[start] = 0;
+  console.timeEnd('edgeHash');
   let queue = [start];
+  let paths = {};
+  paths[start] = 0;
+  console.time('while');
   while (queue.length) {
-    let currentNode = queue.shift();
-    visited[currentNode] = true;
-    if (edgeHash[currentNode]) {
-      edgeHash[currentNode].forEach((n) => {
-        if (!visited[n]) {
-          queue.push(n);
-          distanceHash[n] = distanceHash[currentNode] + 6;
-        }
-      });
-    }
-  }
-  let result = [];
-  for (let i = 1; i <= numOfNodes; i++) {
-    if (i !== start) {
-      if (distanceHash[i]) {
-        result.push(distanceHash[i]);
-      } else {
-        result.push(-1);
+    let current = queue.shift();
+    let nextDist = paths[current] + 6;
+    edgeHash[current].forEach((node) => {
+      if (!paths[node]) { queue.push(node); }
+      if (paths[node] === undefined || paths[node] > nextDist) {
+        paths[node] = nextDist;
       }
+    });
+  }
+  console.timeEnd('while');
+  console.time('result');
+  let result = [];
+  for (let n=1; n<= numOfNodes; n++) {
+    if (n !== start) {
+      result.push(paths[n] === undefined ? -1 : paths[n]);
     }
   }
+  console.timeEnd('result');
   return result.join(" ");
 };
 
