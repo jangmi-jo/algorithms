3bdc5552388ec2d964420541903d86be285d76c5
Jo Jangmi <wkdal720@gmail.com>
2016-10-13 13:32:52 -0700

    coin change problem working on it

---
+++ coding challenges/coin.js
@@ -0,0 +1,23 @@
+let hash = {};
+
+const changes = (n, coins) => {
+  if (n === 0) { return [[]]; }
+  let ways = [];
+  coins.forEach((coin) => {
+    if (n - coin >= 0) {
+      if (hash[n - coin] === undefined) {
+        hash[n - coin] = changes(n - coin, coins).map((change) => {
+          console.log(coin);
+          return change.concat(coin);
+        });
+      }
+      ways = ways.concat(hash[n - coin]);
+    }
+  });
+  console.log(hash);
+  return ways;
+};
+
+
+console.log(changes(2, [3, 2, 1]));
+// console.log(changes(10, [2, 5, 3, 6]));
