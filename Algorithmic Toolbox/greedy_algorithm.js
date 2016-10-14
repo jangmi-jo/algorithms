96414f80e9e9bcf18cc58443f0a009583f27221b
Jo Jangmi <wkdal720@gmail.com>
2016-10-14 10:34:11 -0700

    collect sigature solved

--- Algorithmic Toolbox/greedy_algorithm.js
+++ Algorithmic Toolbox/greedy_algorithm.js
@@ -1,3 +1,7 @@
+const inputHelper = (input) => {
+  return input.split("\n").map((line) => line.split(" ").map(Number));
+}
+
 let denominations = [10, 5, 1];
 
 const changeMoney = (n) => {
@@ -41,9 +45,55 @@ const maximizeValue = (arr) => {
   return value;
 };
 //
-// let t1 = `3 50
+// let t1 = inputHelper(`3 50
 // 60 20
 // 100 50
-// 120 30`.split("\n").map((line) => line.split(" ").map(Number));
-
+// 120 30`);
+//
 // console.log(maximizeValue(t1));
+
+const maximizeRevenue = (a, b) => {
+  // time: O(nlogn) for sorting
+  // space: O(1)
+  a.sort((i, j) => i - j);
+  b.sort((i, j) => i - j);
+  let maxRevenue = 0;
+  for (let i=0; i<a.length; i++) {
+    maxRevenue += a[i] * b[i];
+  }
+  return maxRevenue;
+};
+
+// let t2 = inputHelper(`1 3 -5
+// -2 4 1`);
+//
+// console.log(maximizeRevenue(...t2));
+
+const collectSignature = (arr) => {
+  // for convenience, I made the count in the segments arr
+  // time: O(nlogn)
+  // space: O(n) for the worst case
+  let countSegments = [0];
+  arr.shift();
+  arr.sort((a, b) => a[1] - b[1]);
+  while (arr.length) {
+    let currentSeg = arr.shift();
+    arr = arr.filter((seg) => seg[0] > currentSeg[1] || seg[1] < currentSeg[1]);
+    countSegments[0]++;
+    countSegments.push(currentSeg[1]);
+  }
+  return countSegments;
+};
+//
+// let t3 = inputHelper(`3
+// 1 3
+// 2 5
+// 3 6`);
+//
+// console.log(collectSignature(t3));
+
+const maximizePrize = (n) => {
+
+};
+
+console.log(maximizePrize(6));
