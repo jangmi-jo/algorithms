25ef4ec32decb680532277fe57865e8ff217cb2a
Jo Jangmi <wkdal720@gmail.com>
2016-10-19 18:34:20 -0700

    dynamic array implemented in JS

--- Data Structure/DynamicArray.js
+++ Data Structure/DynamicArray.js
@@ -7,16 +7,81 @@ class DynamicArray {
   }
 
   get(i) {
+    // O(1)
+    this.inBoundValidation(i);
     return this.store[i];
   }
 
   set(i, val) {
-    if (i < this.capacity && i >= 0) {
-      this.store[i] = val;
+    this.inBoundValidation(i);
+    if (this.store[i] === undefined) {
+      // set new element => addition
+      this.ensureLengthAndResize();
+      this.length++;
+    }
+    if (val === undefined) {
+      // set element to undefined => deletion
+      this.length--;
+    }
+    this.store[i] = val;
+  }
+
+  push(val) {
+    // O(1) ammortized
+    this.ensureLengthAndResize();
+    this.set(this.length, val);
+  }
+
+  unshift(val) {
+    // O(n) to move all the elements to right
+    this.ensureLengthAndResize();
+    for (let i=this.length; i>0; i--) {
+      this.set(i, this.get(i-1));
+    }
+    this.set(0, val);
+  }
+
+  shift() {
+    // O(n) to move all the elements to left
+    this.checkEmpty();
+    let returnEl = this.get(0);
+    for (let i=1; i<=this.length; i++) {
+      this.set(i-1, this.get(i));
+    }
+    return returnEl;
+  }
+
+  pop() {
+    // O(1)
+    this.checkEmpty();
+    let returnEl = this.get(this.length - 1);
+    this.set(this.length - 1, undefined);
+    return returnEl;
+  }
+
+  checkEmpty() {
+    if (this.length === 0) {
+      throw 'Store is empty.';
     }
   }
 
+  ensureLengthAndResize() {
+    // O(n) to resize the array
+    if (this.length === this.capacity) {
+      let newStore = new Array(this.capacity * 2);
+      for (let i=0; i<this.capacity; i++) {
+        newStore[i] = this.get(i);
+      }
+      this.capacity *= 2;
+      this.store = newStore;
+    }
+  }
 
+  inBoundValidation(i) {
+    if (i < 0 || i >= this.capacity) {
+      throw 'Index is not in Bound';
+    }
+  }
 }
 
 module.exports = DynamicArray;
--- Data Structure/test_dynamic.js
+++ Data Structure/test_dynamic.js
@@ -1,3 +1,16 @@
 const DynamicArray = require('./DynamicArray.js');
 
 let arr = new DynamicArray();
+arr.push(2);
+arr.push(3);
+arr.unshift(1);
+arr.unshift(0);
+arr.unshift(4);
+arr.unshift(5);
+arr.unshift(6);
+arr.unshift(7);
+arr.unshift(8);
+
+console.log(arr.capacity);
+console.log(arr.store);
+console.log(arr.length);
