5fff9a3fccac143954c81a3ab9b42306c950d0c4
Jo Jangmi <wkdal720@gmail.com>
2016-10-20 21:03:13 -0700

    ring buffer in JS

--- Data Structure/RingBuffer.js
+++ Data Structure/RingBuffer.js
@@ -1,6 +1,89 @@
 class RingBuffer {
   constructor() {
+    this.capacity = 8;
+    this.store = new Array(this.capacity);
+    this.length = 0;
+    this.startIdx = 0;
+  }
 
+  get(i) {
+    // O(1)
+    this.inBoundValidation(i);
+    i = (this.startIdx + i) % this.capacity;
+    return this.store[i];
+  }
+
+  set(i, val) {
+    this.inBoundValidation(i);
+    i = (this.startIdx + i) % this.capacity;
+    if (this.store[i] === undefined) {
+      // set new element => addition
+      this.ensureLengthAndResize();
+      this.length++;
+    }
+    if (val === undefined) {
+      // set element to undefined => deletion
+      this.length--;
+    }
+    this.store[i] = val;
+  }
+
+  push(val) {
+    // O(1) ammortized
+    this.ensureLengthAndResize();
+    this.set(this.length, val);
+  }
+
+  unshift(val) {
+    // O(1)
+    this.ensureLengthAndResize();
+    this.startIdx = (this.startIdx - 1) % this.capacity;
+    if (this.startIdx < 0) { this.startIdx = this.capacity + this.startIdx; }
+    this.set(0, val);
+  }
+
+  shift() {
+    // O(1)
+    this.checkEmpty();
+    let returnEl = this.get(0);
+    this.set(0, undefined);
+    this.startIdx = (this.startIdx + 1) % this.capacity;
+    if (this.startIdx < 0) { this.startIdx = this.capacity + this.startIdx; }
+    return returnEl;
+  }
+
+  pop() {
+    // O(1)
+    this.checkEmpty();
+    let returnEl = this.get(this.length - 1);
+    this.set(this.length - 1, undefined);
+    return returnEl;
+  }
+
+  checkEmpty() {
+    if (this.length === 0) {
+      throw 'Store is empty.';
+    }
+  }
+
+  ensureLengthAndResize() {
+    // O(n) to resize the array
+    if (this.length === this.capacity) {
+      let newStore = new Array(this.capacity * 2);
+      for (let i=0; i<this.capacity; i++) {
+        newStore[i] = this.get(i);
+      }
+      this.capacity *= 2;
+      this.store = newStore;
+      this.startIdx = 0;
+    }
+  }
+
+  inBoundValidation(i) {
+    if (i < 0 || i >= this.capacity) {
+      throw 'Index is not in Bound';
+    }
   }
 }
+
 module.exports = RingBuffer;
--- Data Structure/test_ring_buffer.js
+++ Data Structure/test_ring_buffer.js
@@ -1,1 +1,21 @@
 const RingBuffer = require('./RingBuffer.js');
+
+let arr = new RingBuffer();
+arr.push(2);
+arr.push(3);
+arr.unshift(1);
+arr.unshift(0);
+arr.unshift(-1);
+arr.push(4);
+arr.push(5);
+arr.push(6);
+arr.push(7);
+arr.unshift(-2);
+arr.unshift(-3);
+arr.push(8);
+arr.shift();
+arr.pop();
+
+console.log(arr.capacity);
+console.log(arr.store);
+console.log(arr.length);
