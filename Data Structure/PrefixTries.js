8f7aeb01848dbb8e0ae6accca0a2e656b80bdb92
Jo Jangmi <wkdal720@gmail.com>
2016-10-24 11:32:43 -0700

    prefix trie implemented

--- Data Structure/PrefixTries.js
+++ Data Structure/PrefixTries.js
@@ -1,6 +1,62 @@
+// Key concept was the TrieNode needs the word instead of the each letter
+// construct children as hash for fast lookup
+// needs isLast variable
+
+
+class TrieNode {
+  constructor(word) {
+    this.word = word;
+    this.isLast = false;
+    this.children = {};
+  }
+}
+
 class PrefixTrie {
   constructor() {
+    this.root = new TrieNode("");
+  }
 
+  insert(word) {
+    // time: O(n) where n is word's length
+    // space: O(n)
+    let currentNode = this.root;
+    for (let i=0; i<word.length; i++) {
+      if (currentNode.children[word[i]] !== undefined) {
+        currentNode = currentNode.children[word[i]];
+      } else {
+        let newNode = new TrieNode(currentNode.word + word[i]);
+        currentNode.children[word[i]] = newNode;
+        currentNode = newNode;
+      }
+      if (i === word.length - 1) {
+        currentNode.isLast = true;
+      }
+    }
+  }
+
+  search(word) {
+    // time: O(n), space: O(1)
+    let currentNode = this.root;
+    for (let i=0; i<word.length; i++) {
+      if (currentNode.children[word[i]]) {
+        currentNode = currentNode.children[word[i]];
+      } else {
+        return false;
+      }
+    }
+    return currentNode.isLast;
+  }
+
+  startsWith(pre) {
+    let currentNode = this.root;
+    for (let i=0; i<pre.length; i++) {
+      if (currentNode.children[pre[i]]) {
+        currentNode = currentNode.children[pre[i]];
+      } else {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
--- Data Structure/test_prefix_tries.js
+++ Data Structure/test_prefix_tries.js
@@ -1,3 +1,21 @@
 const PrefixTrie = require('./PrefixTries.js');
 
 let trie = new PrefixTrie();
+
+trie.insert('leet');
+// trie.insert('abc');
+//
+// console.log(trie.search('dfg'));
+//
+
+
+
+console.log(trie);
+let l = trie.root.children['l'];
+console.log(l);
+let le = l.children['e'];
+console.log(le);
+let lee = le.children['e'];
+console.log(lee);
+let leet = lee.children['t'];
+console.log(leet);
