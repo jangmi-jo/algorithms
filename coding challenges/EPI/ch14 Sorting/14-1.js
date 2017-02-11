dd08c13be37ab063e52f4cbfd4f63fa1efd266e0
Jo Jangmi <wkdal720@gmail.com>
2017-02-11 08:59:18 -0800

    a bit better

--- coding challenges/EPI/ch14 Sorting/14-1.js
+++ coding challenges/EPI/ch14 Sorting/14-1.js
@@ -24,13 +24,13 @@ const intersection = (a, b) => {
   const res = [];
   let i = 0;
   let j = 0;
-  while (i < a.length - 1 || j < b.length - 1) {
+  while (i < a.length && j < b.length) {
     while (a[i-1] === a[i]) { i++; }
     while (b[j-1] === b[j]) { j++; }
     if (a[i] === b[j]) {
       res.push(a[i]);
-      if (i < a.length - 1) { i++; }
-      if (j < b.length - 1) { j++; }
+      i++;
+      j++;
     } else if (a[i] < b[j]) {
       i++;
     } else {
@@ -41,7 +41,7 @@ const intersection = (a, b) => {
 };
 
 
-const a = [2, 3, 3, 5, 5, 6, 7, 7, 8, 12];
+const a = [2, 3, 3, 5, 5, 6, 7, 7, 8, 9, 9, 9, 9, 10, 12];
 const b = [5, 5, 6, 8, 8, 9, 10, 10];
 // res = [5, 6, 8]
 
